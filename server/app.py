4f0243f47e2104a18727ebab7adbbf3cdae1a2ad
Kevin Kao <kk415kk@gmail.com>
2014-10-03 15:18:12 -0700

    Fixed CORS problem

--- server/app.py
+++ server/app.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
-from flask_cors import *
 import shelve
 from subprocess import check_output
 import flask
 from flask import request
 from os import environ
 import string, random
-
-import json
-from datetime import timedelta  
 from functools import wraps, update_wrapper
 from flask import Flask, redirect, request, current_app, make_response
 
@@ -20,55 +16,14 @@ short_to_long_db = shelve.open("stl.db")
 long_to_short_db = shelve.open("lts.db")
 BASE_URL = "scribble.ly/"
 
-def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):  
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
-
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
-@crossdomain(origin='*')
 def home():
   """Builds a template based on a GET request, with some default
   arguments"""
   return flask.render_template('home.html', title="Home")
 
-@app.route('/shorts', methods=['POST', 'OPTIONS'])
-@crossdomain(origin='*')
+@app.route('/shorts', methods=['POST'])
 def shorten_submission():
   l_url = str(request.form['url'])
   s_url = shorten_url(l_url)
@@ -83,10 +38,6 @@ def shorten_url(l_url):
   long_to_short_db[l_url] = s_url
   short_to_long_db[s_url] = l_url
   return s_url
-
-@app.route('/shorts', methods=['GET'])
-def fetch_all_url_mappings():
-  return flask.render_template('home.html', title="Shorts")
 
 @app.route('/short/<name>', methods=['GET'])
 def fetch_url_mapping(name):
